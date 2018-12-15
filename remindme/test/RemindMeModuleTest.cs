17b4f04732b174bd3072ebe5eedb90a0bd9baedc
Frederik Rosenberg <fsr1998@hotmail.com>
2018-12-15 21:31:10 +0100

    More moving files

---
+++ remindme/test/RemindMeModuleTest.cs
@@ -0,0 +1,15 @@
+using Lomztein.Moduthulhu.Core.Bot.Client.Sharding;
+using Moq;
+using Xunit;
+
+namespace Moduthulhu.Module.Remindme.Test
+{
+    public class RemindMeModuleTest
+    {
+        [Fact]
+        public void UpdateShouldRemoveIfOverTime() 
+        {
+            //TODO
+        }
+    }
+}
